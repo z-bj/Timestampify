4e84092537f7cb3984f4f261d941b78d0757f201
Zakaria Beji <zbjarchi@gmail.com>
2023-03-13 07:55:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -42,27 +42,44 @@ app.get("/api", (req, res) => {
   });
 });
 
-// This sets up an API endpoint at "/api/:date?" that handles requests with a date string. It first checks if the date string is in UNIX timestamp format or a string format. If it is in string format, it attempts to parse it into a UNIX timestamp and returns the corresponding UTC time. If the date string is invalid, it returns an error message.
+/---------------------------------/
 
 app.get("/api/:date?", (req, res) => {
+  // Get the date string from the request parameter
   const dateString = req.params.date;
+  // Regular expression to check if the date string only contains numbers
   const dateStringRegex = /^[0-9]+$/;
+  // Test if the date string contains only numbers
   const numbersOnly = dateStringRegex.test(dateString);
 
+  // If the date string does not contain only numbers
   if (!numbersOnly) {
+    // Parse the date string into a Unix timestamp
     const unixTimestamp = Date.parse(dateString);
+    // Convert the Unix timestamp to a UTC date string
     const utcDate = new Date(unixTimestamp).toUTCString();
 
+    // If the Unix timestamp is valid, send a JSON response with the Unix timestamp and UTC date string
+    // Otherwise, send a JSON response with an error message
     unixTimestamp
       ? res.json({ unix: unixTimestamp, utc: utcDate })
       : res.json({ error: "Invalid Date" });
-  } else {
+  } 
+  // If the date string contains only numbers
+  else {
+    // Parse the date string as a Unix timestamp
     const unixTimestamp = parseInt(dateString);
+    // Convert the Unix timestamp to an actual date
     const actualDate = new Date(unixTimestamp);
+    // Convert the actual date to a UTC date string
     const utcDate = actualDate.toUTCString();
+    // Send a JSON response with the Unix timestamp and UTC date string
     res.json({ unix: unixTimestamp, utc: utcDate });
   }
 });
+
+
+// -------------------------------------------------------------------
 
 // This starts the server on the specified port (either from the environment variable or port 3000 if it's not defined) and logs a message to the console when it's running.
 
